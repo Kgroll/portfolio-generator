dd99c1baa48b26dfd84c4d1e080559d9c357ad9d
Kristen Groller <groller@rogers.com>
2021-02-04 21:34:00 -0500

    added detail to html file

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const inquirer = require('inquirer');
 
-//const fs = require('fs');
+const fs = require('fs');
 
-//const generatePage = require('./src/page-template.js');
+const generatePage = require('./src/page-template.js');
 
 //personal questions
 const promptUser = () => {
@@ -47,7 +47,7 @@ if (!portfolioData.projects) {
     {
       type: 'input',
       name: 'description',
-      message: 'Provide a dexcription of the project (Required)'
+      message: 'Provide a description of the project (Required)'
     },
     {
       type: 'checkbox',
@@ -89,19 +89,20 @@ if (!portfolioData.projects) {
 promptUser()
     .then(promptProject)
     .then(portfolioData =>{
-      console.log(portfolioData);       
+      
+ const pageHTML = generatePage(portfolioData);
+
+
+fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw new Error(err);
+
+  //console.log('Page created! Check out index.html in this directory to see it!');
+//});
     });
 
-  
-
-//const pageHTML = generatePage(name, github);
+    })
 
 
-//fs.writeFile('./index.html', pageHTML, err => {
- // if (err) throw err;
-
-  //console.log('Portfolio complete! Check out index.html to see the output!');
-//});
 
 
 
